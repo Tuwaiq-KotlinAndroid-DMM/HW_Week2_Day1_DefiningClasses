53fa69051e199db12cf8bb0612e2110d9f5b1db7
abdulrazaqahmed <Abdulrazaq.Alsomali.dmm.k1@tuwaiq.edu.sa>
2021-10-04 02:06:41 +0300

    Week2Day1Hw_Done

---
+++ solutionkt/src/main/kotlin/Main.kt
@@ -0,0 +1,66 @@
+//Solution Part1 "myCar"
+//class Car()
+/*{
+    var model : String ="Toyota"
+    var manifacturedYear : Int = 2020
+
+
+}
+
+
+
+//Solution Part2 "book"
+class Book(name: String, author:String, price:Double, availability:Boolean)
+{
+     var name : String = name
+     var author : String = author
+     var price : Double = price
+     var availability : Boolean = availability
+        fun bookdetails(){
+            println(".....Book details.....")
+            println("$name")
+            println("$author")
+            println(price)
+            println(availability)
+
+        }
+
+
+
+}
+
+
+ */
+//Solution part3
+
+fun main() {
+    println("Enter the length : ")
+    var length = readLine()!!.toInt()
+    println("Enter the width: ")
+    var wigth = readLine()!!.toInt()
+    var x = Rectangle(length,wigth)
+    println( "perimeter: ${x.getPer()}")
+    println("area : ${x.getArea()}")
+
+    println(x.draw())
+}
+class Rectangle (_length : Int , _width :Int){
+    var length : Int = _length
+    var width : Int = _width
+    fun getPer() : Int{
+        return 2 * (length + width)
+    }
+    fun getArea(): Int{
+        return length * width
+    }
+    fun draw( ):String{
+        for (A in 1.. width){
+            for (E in 1 .. length){
+                print("* ")
+            }
+            println("")
+
+        }
+        return ("")
+    }
+}
