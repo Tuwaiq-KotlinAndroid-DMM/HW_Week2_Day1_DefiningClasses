9f1b97d15c4386e5c3afaaac1604aeea431e6fcf
waad <iwaad1414@gmail.com>
2021-10-03 20:36:32 +0300

    week2day1class

--- solutionktclasspart2/src/main/kotlin/Main.kt
+++ solutionktclasspart2/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import jdk.jfr.BooleanFlag
 
 class Book(title: String, price: Double, author:String, availability:Boolean) {
-var title:String="زهايمر"
-    var author:String="غازي القصيبي"
-    var price:Double=20.50
+var title:String=title
+    var author:String=author
+    var price:Double=price
    var availability:Boolean=availability
     fun bookdetails() {
         println(title)
@@ -11,11 +11,16 @@ var title:String="زهايمر"
         println(author)
         if(availability==true)
             println("inStock")
-        }
+        else             println("out of Stock")
+
+    }
     }
 fun main(){
     var book1=Book("زهايمر",20.50,"غازي القصيبي",true)
+    var book2=Book("حياة في الإدارة",40.50,"غازي القصيبي",false)
+
     println(book1.bookdetails())
+    println(book2.bookdetails())
 
 
 }
