1d99874ef5b1199e648be66b7d48778b2330bd30
Saad alhajri <saadalhajri@Saads-MacBook-Air.local>
2021-10-04 07:48:10 +0300

    Day6HW

---
+++ Solution/src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+//fun main(args: Array<String>){
+//    Part I
+//class Car(model:String, year:Int) {
+//
+//        var model: String = model
+//        var year: Int = year
+//    }
+//
+//    var myCar = Car("Toyota",2020)
+//
+//    println("The model of this car is:" + myCar.model)
+//        println("The year of this car is:" + myCar.year)
+//}
+
+//
+//
+////       Part II
+//
+
+
+//class book(title: String, author: String, price: Double, availability: Boolean) {
+//
+//    var title = title
+//    var author = author
+//    var price = price
+//    var availability = availability
+//    fun bookdetails() {
+//        println("--- Book Details ---")
+//        println("$title")
+//        println("$author")
+//        println(price)
+//        if (availability == true)
+//            println("inStock")
+//        else println("out of Stock")
+//    }
+//}
+//fun main() {
+//    var book1 = book("Game of Thrones", "George Martin", 54.00, true)
+//    var book2 = book("We Were Liars", "E. Lockhart", 30.00, false)
+//
+//    println(book1.bookdetails())
+//    println(book2.bookdetails())
+//}
