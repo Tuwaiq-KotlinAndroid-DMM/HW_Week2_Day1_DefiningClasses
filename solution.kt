eac83521d68a79086823d52acdd6701c8e60a37e
Arwa Saleh Alomari <91452271+ArwaSAO@users.noreply.github.com>
2021-10-04 05:30:10 +0300

    Add files via upload

---
+++ solution.kt
@@ -0,0 +1,43 @@
+//HOME WORK 1 -WEEK 2
+// question 1 part1
+//PART1
+
+/*class Car(){
+    var car:String = "Toyota"
+    var year:Int = 2020
+}
+fun main () {
+    var car = Car()
+    println("Model: ${car.car}  Year: ${car.year}")
+}*/
+
+//PART 2
+
+/*fun main(){
+    var thebook = book()
+    print(thebook.bookDetails())
+    println("\n")
+    var theSecbook = book()
+    theSecbook.title = "We Were Liars"
+    theSecbook.author = "E. Lockhart"
+    theSecbook.price = 30.0
+    theSecbook.inStock = false
+
+    println(theSecbook.bookDetails())
+}
+
+class book() {
+    var title: String = "Game of Thrones"
+    var author: String = "George Martin"
+    var price: Double =  54.00
+    var inStock:Boolean = true
+
+
+    fun bookDetails():String{
+
+        return "The book title : ${title} \n The author name: ${author} \n The price: $ ${price} ,\n Availability : ${inStock()} \n"
+    }
+    fun inStock():Boolean {
+        return inStock
+    }
+}*/
